6a8b0d8a15b784a279d44a478224c631a04a260c
Aanchalt14 <aanchalt71@gmail.com>
2021-01-18 11:57:42 +0530

    Add : Validate last name regex pattern

--- UserRegistration1.java
+++ UserRegistration1.java
@@ -1,6 +1,64 @@
-public class UserRegistration {
+package UserRegistration.com;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class UserRegistration1 {
 
 	 public static void main(String[] args) {
 	        System.out.println("Welcome to UserRegistration");
-  }
+	        int choose = 1;
+	        for(choose=1;choose<4;choose++) {
+
+	            switch (choose) {
+	                case 1:
+	                    UserRegistrationOptions.firstName();
+	                    break;
+                        case 2:
+	                    UserRegistrationOptions.lastName();
+	                    break;
+	             }
+	        }
+	    }
+	}
+
+  class UserRegistrationOptions{
+  static Scanner input = new Scanner(System.in);
+  static String text;
+  static String firstName = "^[A-Z][a-zA-Z]{2,}$";
+ 
+  public static void firstName() {
+       while (true) {
+        System.out.println("Enter FirstName : ");	
+        text = input.nextLine();
+        Pattern pattern = Pattern.compile(firstName);
+        Matcher matcher = pattern.matcher(text);
+	      if (matcher.matches()) {
+	          System.out.println("Valid pattern");
+	            break;
+	       }
+              else {
+	       System.out.println("Invalid firstName Pattern,Try Again");
+	       System.out.println(" ");       }
+	       }
+     }
+}
+
+
+   //UC-2
+  public static void lastName() {
+	        while (true) {
+	            System.out.println("Enter lastName : ");
+	            text = input.next();
+	            Pattern pattern = Pattern.compile(lastName);
+	            Matcher matcher = pattern.matcher(text);
+	            if (matcher.matches()) {
+	                System.out.println("Valid pattern");
+	                break;
+	            }
+                    else {
+	                System.out.println("Invalid Pattern,Try Again");
+	                System.out.println(" ");
+	            }
+	       }
+
 }
